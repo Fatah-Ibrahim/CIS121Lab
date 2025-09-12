26fec0b161c45e3dfe5760c3b6b864c402e89fcc
Fatah Ibrahim <ze5141be@go.minnstate.edu>
2025-09-12 11:26:27 -0500

    did Question 3

--- Week 3/week3.py
+++ Week 3/week3.py
@@ -1,1 +1,12 @@
-return'''===sadfiuhasidfu'''
+#question 3
+light_color = input("Enter a light color: ").lower()
+
+
+if light_color == "green":
+    print("Go")
+elif light_color == "yellow":
+    print("Yeild")
+elif light_color == "red":
+    print("Stop")
+else:
+    print("Invalid Color")
